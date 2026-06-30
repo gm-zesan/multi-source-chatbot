f961000386604182ffca8c03db0d421ff1d87fe9
gm-zesan <gmzesan7767@gmail.com>
2026-06-30 19:26:02 +0600

    Implement Facebook Messenger response handling in WebhookController

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Http;
 
 class WebhookController extends Controller
 {
@@ -27,6 +28,26 @@ class WebhookController extends Controller
             'body'    => $request->getContent(),
             'json'    => $request->all(),
         ]);
+
+        $messaging = $request->input('entry.0.messaging.0');
+
+        $recipientId = $messaging['sender']['id'] ?? null;
+        $messageText = $messaging['message']['text'] ?? '';
+
+        if ($recipientId) {
+            $response = Http::withToken(env('FB_PAGE_ACCESS_TOKEN'))
+                ->post('https://graph.facebook.com/v25.0/me/messages', [
+                    'recipient' => [
+                        'id' => $recipientId,
+                    ],
+                    'message' => [
+                        'text' => 'Laravel received: ' . $messageText,
+                    ],
+                ]);
+
+            Log::info('Facebook Send API Response', $response->json());
+        }
+
 
         return response('EVENT_RECEIVED', 200);
     }
