83ea39da0b959a1c8a6b0527764750176d60d229
gm-zesan <gmzesan7767@gmail.com>
2026-07-01 17:49:58 +0600

    fix: Update ChannelAccountSeeder to correctly reference channel_id from Channel model

--- database/seeders/ChannelAccountSeeder.php
+++ database/seeders/ChannelAccountSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Channel;
 use App\Models\ChannelAccount;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,13 +14,14 @@ class ChannelAccountSeeder extends Seeder
      */
     public function run(): void
     {
+        $facebook = Channel::where('slug', 'facebook')->firstOrFail();
         ChannelAccount::updateOrCreate(
             [
-                'external_id' => '1229035226952947',
+                'channel_id'    => $facebook->id,
+                'external_id'   => '1229035226952947',
             ],
             [
                 'workspace_id'  => 1,
-                'channel_id'    => 1,
                 'name'          => 'facebook_page',
                 'display_name'  => 'Entrepreneurs Automation',
                 'access_token'  => env('FB_PAGE_ACCESS_TOKEN'),
